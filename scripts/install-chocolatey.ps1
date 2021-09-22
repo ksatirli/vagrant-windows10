ab0aa2a0fbbc093109e0549b6af94130c567188b
Kerim Satirli <ksatirli@users.noreply.github.com>
2021-09-22 16:56:13 +0200

    Adds Vagrantbox (#1)
    * adds Vagrantfile
    
    * adds PowerShell scripts
    
    * adds Makefile for convenience
    
    * adds README

---
+++ scripts/install-chocolatey.ps1
@@ -0,0 +1,2 @@
+# see https://docs.chocolatey.org/en-us/choco/setup#install-with-powershell.exe
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
---
+++ scripts/install-windows-updates.ps1
@@ -0,0 +1,2 @@
+Install-Module -Name PSWindowsUpdate -Force
+Get-WUInstall -MicrosoftUpdate -AcceptAll -IgnoreReboot
